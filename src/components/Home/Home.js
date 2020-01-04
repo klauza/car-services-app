4792ffbbac35091be0a7d105b2ca20908839f025
klauza <klauza.uk@gmail.com>
2020-01-04 18:35:46 +0000

    populate project with new sketch-components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './About';
 import Hero from './Hero';
+import Parallax from './Parallax';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Parallax />
       <About />
     </div>
   )
--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -11,6 +12,13 @@ const Wrapper = styled.div`
 const Navbar = () => {
   return (
     <Wrapper>
+      <ul>
+        <li><NavLink to="/">&#9776; Home</NavLink></li>
+        <li><NavLink to="/services">Services</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to="/map">Map</NavLink></li>
+        <li><NavLink to="/frequently-asked-questions">FAQ</NavLink></li>
+      </ul>
       nav
     </Wrapper>
   )
