08784eaf9176485b28fa5c948122d16a1de1253c
klauza <klauza.uk@gmail.com>
2020-01-06 21:28:43 +0000

    create Contect layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,91 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { contactBg } from '../../media';
+
+const Wrapper = styled.div`
+  height: calc( 100vh - 50px );
+  background: url( ${props => props.bg} ) no-repeat;
+  background-position: 50% 20%;
+  background-size: cover;
+  position: relative;
+
+  .contact-container{
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: auto;
+
+    .support-div{
+      max-width: 1280px;
+      width: auto;
+      margin: 0 auto;
+      border: 1px solid black;
+
+      display: grid; grid-template-columns: 1fr 1fr;
+      align-content: center;
+    }
+
+    &__options{
+      width: 100%; height: 100%;
+      background: red;
+      .flex-row-option{
+        padding: 15px 5px;
+        display: flex; flex-direction: row;
+        &__content{
+          margin-left: 5px;
+          display: grid;
+          align-content: center;
+        }
+      }
+      
+    }
+
+    &__desc{
+      width: 100%; height: 100%;
+      background: white;
+    }
+  }
+`;
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <Wrapper bg={contactBg}>
+      <div className="contact-container">
+        <div className="support-div">
+          <div className="contact-container__options">
+            <div className="flex-row-option">
+              <img src="http://placekitten.com/150/150" alt=""/>
+              <div className="flex-row-option__content">
+                <h2>Address</h2>
+                <p>address line 1</p>
+                <p>address post code</p>
+              </div>
+            </div>
+
+            <div className="flex-row-option">
+              <img src="http://placekitten.com/150/150" alt=""/>
+              <div className="flex-row-option__content">
+                <h2>Phone</h2>
+                <p>+44 9559421852</p>
+              </div>
+            </div>
+
+            <div className="flex-row-option">
+              <img src="http://placekitten.com/150/150" alt=""/>
+              <div className="flex-row-option__content">
+                <h2>Map</h2>
+                <button>Check map</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact-container__desc">
+            2
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
--- src/media/index.js
+++ src/media/index.js
@@ -1,2 +1,3 @@
 export { default as background } from './images/background.jpeg';
+export { default as contactBg } from './images/contactBg.jpeg';
 export { default as boxServicesImage } from './images/box-services-image.jpg';
