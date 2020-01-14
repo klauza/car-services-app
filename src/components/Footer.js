1d02cacd0ea34147fda5f1f05525c4338fb80056
klauza <klauza.uk@gmail.com>
2020-01-14 00:50:19 +0000

    footer edit: visible for desktop only

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { BrowserView } from "react-device-detect";
 
 const FooterWrap = styled.div`
   background: rgba(0,0,0,.35);
   padding: 12.5px 0;
-  margin: 0 auto 50px;
+  margin: 0 auto;
   text-align: center;
   color: #fff;
-  margin-top: auto;
+  /* margin-top: auto; */
   width: 100%;
 
   @media(max-width: 768px){
     /* display: none; */
+  margin: 0 auto 50px;
   }
 `;
 const Footer = () => {
   return (
-    <FooterWrap>
-      FOOTER
-    </FooterWrap>
+    <BrowserView style={{marginTop: 'auto'}}>
+      <FooterWrap>
+        <NavLink to="/about">About</NavLink>
+        FOOTER -- Website created by Michal Klauza
+        © Copyright 2020
+      </FooterWrap>
+    </BrowserView>
   )
 }
 
