7f4f2273071d67e91513e7a32b6993059884c809
klauza <klauza.uk@gmail.com>
2020-01-14 21:11:45 +0000

    fix display for mobile

--- src/components/Navigation/MobileBottomNavigation.js
+++ src/components/Navigation/MobileBottomNavigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { Home, Tools, SpeechBubble, Help } from '../../Icons/index';
+import { Home, Tools, SpeechBubble, Help } from '../../Icons';
 
 const Wrapper = styled.div`
   z-index: 998;
@@ -11,68 +11,67 @@ const Wrapper = styled.div`
   height: 50px;
   background: #433838;
 
-
-  ul{
-    height: 100%;
+  &>div{
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 50px;
   
 
-    li{
-      list-style: none;
-      width: 100%; height: 100%;
-      display: grid; 
-      text-align: center;
-      align-content: flex-start;
-      border-top: 1px solid #fff; 
-      &:not(:nth-child(4)){ border-right: 1px solid #fff; }
+ 
+
+      
 
 
       a{
         display: flex; flex-direction: column;
-        line-height: 50px;
+        align-content: flex-start;
+        /* height: 50px; */
         text-decoration: none;
         color: #fff;
         font-family: 'Oswald', sans-serif;
         letter-spacing: 1.25px;
+        border-top: 1px solid #fff; 
+        /* list-style: none; */
+        width: 100%; height: 100%;
+        &:not(:nth-child(4)){ border-right: 1px solid #fff; }
 
         svg{
-          /* fill: rgba(255,255,255,.5); */
           fill: #fff;
           opacity: .65;
           flex: 1;
           width: 25px;
-          height: auto;
-          display: block;
+          height: 25px;
+          /* display: block; */
           margin: 3.5px auto 0;
           transition: fill .5s ease;
         }
-        svg path, svg circle{
+        svg path, 
+        svg circle{
+          fill: #fff;
           opacity: .65;
-          /* fill: rgba(255,255,255,.5); */
         }
+
         &.active svg{
           opacity: 1;
           transition: opacity .5s ease;
           path, circle{
-              /* fill: #fff; */
               opacity: 1;
               transition: opacity .5s ease;
             }
         }
 
         span{
-          margin-top: -15px;
+          margin-top: -2.5px;
           flex: 1;
           transition: color .5s ease;
           color: rgba(255,255,255,.65);
+          text-align: center;
         }
         &.active span{
           color: #fff;
           transition: color .5s ease;
         }
       }
-    }
 
   }
 
@@ -82,12 +81,12 @@ const MobileBottomNavigation = () => {
   return (
     <Wrapper>
 
-      <ul>
-        <li><NavLink exact to="/"> <Home /><span>Home</span></NavLink></li>
-        <li><NavLink exact to="/services"> <Tools /><span>Services</span> </NavLink></li>
-        <li><NavLink exact to="/contact"> <SpeechBubble /><span>Contact</span></NavLink></li>
-        <li><NavLink exact to="/frequently-asked-questions"> <Help /><span>FAQ</span></NavLink></li>
-      </ul>
+      <div>
+        <NavLink exact to="/"> <Home /><span>Home</span></NavLink>
+        <NavLink exact to="/services"> <Tools /><span>Services</span> </NavLink>
+        <NavLink exact to="/contact"> <SpeechBubble /><span>Contact</span></NavLink>
+        <NavLink exact to="/frequently-asked-questions"> <Help /><span>FAQ</span></NavLink>
+      </div>
 
     </Wrapper>
   )
